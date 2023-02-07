91c8cd99212256b72f74d98ae4bf8fd6eb319346
VIIBII <birlavishal90@gmail.com>
2023-02-08 00:39:36 +0530

    todo-app

---
+++ src/components/TodoForm.js
@@ -0,0 +1,67 @@
+import React,{useState, useEffect, useRef} from 'react'
+
+function TodoForm(props) {
+
+    const[input, setInput]=useState('')
+
+    const inputRef =useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus();
+      });
+
+    const handleChange = e =>{
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random()*10000),
+            text: input
+        })
+
+        setInput('')
+    }
+
+  return (
+    <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+        <div className='todo-bottom'>
+          <input
+            placeholder='Update your item'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Update
+          </button>
+          </div>
+        </>
+      ) : (
+        <>
+         <div className='todo-top'>
+          <input
+            placeholder='Add a todo'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Add 
+          </button>
+          </div>
+        </>
+      )}
+    </form>
+  );
+}
+
+export default TodoForm
